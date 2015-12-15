16f75dc654d78646ebb9738f42392e5fbfb29103
Konstantine <kostya.stanev@gmail.com>
2015-12-15 06:27:16 +0200

    List fixed, no errors

---
+++ GlobalClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskList
+{
+    public static class Class1
+    {
+        public static List<Task> taskList = new List<Task>();
+        public Class1()
+        {
+        }
+    }
+}
